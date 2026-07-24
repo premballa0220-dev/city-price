6d16105cfb556ce78317ca4b550bdf2b0c18a2c5
premballa0220-dev <premballa0220@gmail.com>
2026-07-24 17:48:44 +0530

    Add files via upload

--- City_price/server.js
+++ City_price/server.js
@@ -1,1 +1,26 @@
-module.exports = require('./shopify-city-pricing/server');
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+app.get('/', (req, res) => {
+  res.send('City price backend is running');
+});
+
+const PORT = Number(process.env.PORT || 3000);
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
